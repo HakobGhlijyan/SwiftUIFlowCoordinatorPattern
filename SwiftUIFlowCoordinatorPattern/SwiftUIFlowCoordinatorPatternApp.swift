0acbb615835291824a1e15923e3b90506fb971b4
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-06-11 14:30:09 +0400

    Initial Commit

---
+++ SwiftUIFlowCoordinatorPattern/SwiftUIFlowCoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFlowCoordinatorPatternApp.swift
+//  SwiftUIFlowCoordinatorPattern
+//
+//  Created by Hakob Ghlijyan on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFlowCoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
